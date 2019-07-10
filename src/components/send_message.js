8a8a6e8bffbe9ce831c521d7872b36ef3c802615
Tashane Mclean <tashane.mclean@gmail.com>
2019-07-10 09:55:55 -0400

    added bot typing to response state

--- src/components/send_message.js
+++ src/components/send_message.js
@@ -1,7 +1,6 @@
 import React , {Component} from 'react'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import { faArrowAltCircleRight, faShareSquare} from "@fortawesome/fontawesome-free-regular"
-import ImageUpload from './imageUpload';
+import {faShareSquare, faCommentDots} from "@fortawesome/fontawesome-free-regular"
 
 
 export default class SendMessage extends Component {
@@ -9,26 +8,31 @@ export default class SendMessage extends Component {
         super(props)
 
         this.state = {
-            usrInput: "",
+            // usrInput: "",
+            image: "../../static/assets/images/botImage.jpg",
+            typing: false,
             bot: {
-                image: "../../static/assets/images/botImage.jpg",
                 botResponse: "Hi, I’m Emma. A Watson Assistant. I can provide you with account info and bank hours & locations."
             },
             allInput: []
         }
-        this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.inputAndResponseToArray = this.inputAndResponseToArray.bind(this)
+        this.clearInput = this.clearInput.bind(this)
     }
 
-    handleChange(e) {
-        event.preventDefault()
-        this.setState({[e.target.name]: e.target.value})
+    clearInput(){
+        const usrInput = this.refs.usrInput.value=""
     }
 
     handleSubmit(event) {
         event.preventDefault()
-        const usrInput = this.state.usrInput
+        const usrInput = this.refs.usrInput.value
+
+        this.chatLogs()
+        this.clearInput()
+        this.setTyping()
+        
     
         fetch ("http://localhost:5000/sendmessage", {
             method: "POST",
@@ -38,126 +42,151 @@ export default class SendMessage extends Component {
             body:JSON.stringify({usrInput: usrInput})
         })
         .then(response => {return response.json()})
-        .then(response => {this.setState({
-            bot: {botResponse: response.output.text[0]}
-        })})
+        .then(response => {
+            this.setState({
+                bot: {botResponse: response.output.text[0]}
+            })
+        })
         .catch(error => {
             console.log("Fetch Error", error)
         })
     }
 
+    typingInterval = () => {
+        this.typeInt = setInterval(() => {
+            (this.setState({typing: true}))
+            }, 1)
+    }
+
+    notTypingInterval = () => {
+       this.notTypeInt = setInterval(() => {
+            (this.setState({typing: false}))               
+            }, 6000)
+    }
+
+    clearIntervals =()=> {
+        setInterval(()=> {
+            clearInterval(this.typeInt, this.NotTypeInt)
+        }, 6000)
+    }
+
+    setTyping = () => {
+        this.typingInterval()
+        this.notTypingInterval()
+        this.clearIntervals()
+    }
+
     inputAndResponseToArray(){
-        const usrInput = this.state.usrInput
+        const usrInput = this.refs.usrInput.value
         const x = this.state.allInput
         const y = this.state.bot
         x.push(y)
         x.push({
             usrInput: `${usrInput}`
         })
-
     }
 
-    chatLogs = () =>{
+    chatLogs = () => {
         this.inputAndResponseToArray()
     }
 
+    botTyping = () => {
+            return <p className="comment-span" ><FontAwesomeIcon icon={faCommentDots}/></p>
+        }
+    
+    botNotTyping = () =>{
+        return <p className="bot-response">{this.state.bot.botResponse}</p>
+    }
+
     render() {
-        const image = this.state.bot.image
+        const image = this.state.image
         return(
-            <div className="chat-wrapper">
-                <div className="chat">
-                    <div className="bot-profile-name">
-                        <p className="bot-profile-header">Emma | Chatbot Assistant</p>
-                    </div>
-                        <form onSubmit={this.handleSubmit}>
-                            {Object.values(this.state.allInput).map(rec => (
-                              <div className="chat-two">
-                                  <div className="bot-profile-wrapper_two"
-                                  style={{
-                                      "margin": "14px"
-                                  }}
-                                  >
-                                    <div className="bot-image-wrapper_two">
-                                        <img style={{
-                                            'height': '30px' ,
-                                            'width': '30px', 
-                                            'borderRadius': '20px'
-                                            }} src={rec.image}
-                                        />
-                                     </div>
-                                    <div className="chat-box-response-wrapper-two"
+            <div className="chat">
+                <div className="bot-profile-name">
+                    <p className="bot-profile-header">Emma | Chatbot Assistant</p>
+                </div>
+                    <form onSubmit={this.handleSubmit}>
+                        {Object.values(this.state.allInput).map((rec, _id) => (
+                            <div key={_id} className="chat-two">
+                                <div className="bot-profile-wrapper_two"
+                                style={{
+                                    "margin": "14px"
+                                }}
+                                >
+                                <div className="chat-box-response-wrapper-two"
+                                style={{
+                                    "marginLeft": "35px",
+                                    "marginBottom": "20px",
+                                    "backgroundColor": "#afece7",
+                                    "paddingRight": "30px",
+                                    "borderRadius": "12px"
+                                }}
+                                >
+                                    <p className="bot-response_two"
                                     style={{
-                                        "marginLeft": "35px",
-                                        "marginBottom": "20px",
-                                        "backgroundColor": "#afece7",
-                                        "paddingRight": "30px",
-                                        "borderRadius": "12px"
+                                        "margin": "8px",
+                                    "fontSize": "1.3rem",
+                                    "color": "#706c61",
                                     }}
-                                    >
-                                        <p className="bot-response_two"
-                                        style={{
-                                            "margin": "8px",
-                                        "fontSize": "1.3rem",
-                                        "color": "#706c61",
-                                        }}
                                         >{rec.botResponse}
-                                        </p>
-                                    </div>
-                                    <div className="user-input-wrapper"
+                                    </p>
+                                </div>
+                                <div className="user-input-wrapper"
+                                style={{
+                                    "marginLeft": "35px",
+                                    "marginBottom": "20px",
+                                    "backgroundColor": "#e9e9e9",
+                                    "paddingRight": "30px",
+                                    "borderRadius": "12px",
+                                }}
+                                >
+                                    <p className="user-input"
                                     style={{
-                                        "marginLeft": "35px",
-                                        "marginBottom": "20px",
-                                        "backgroundColor": "#e9e9e9",
-                                        "paddingRight": "30px",
-                                        "borderRadius": "12px"
+                                        "margin": "8px",
+                                        "fontSize": "1.3rem",
+                                        "color": "rgb(112, 108, 97)"
                                     }}
-                                    >
-                                        <p className="user-input"
-                                        style={{
-                                            "margin": "8px",
-                                            "fontSize": "1.3rem",
-                                            "color": "rgb(112, 108, 97)"
-                                        }}
-                                        >{rec.usrInput} {console.log(rec)}
-                                        </p>
-                                    </div>
-                                  </div>
-                              </div>
-                            ))}
-                            <div className="input-wrapper">
-                                <div className="input-text-wrapper">
-                                    <a className="submit-icon" 
-                                    onClick={this.handleSubmit}
-                                    >
-                                    <FontAwesomeIcon 
-                                    onClick={this.chatLogs}
-                                    icon={faShareSquare}
-                                    />
-                                    </a>
-                                    <textarea 
-                                    className="input-box" 
-                                    type="text" name="usrInput" 
-                                    value={this.state.usrInput} 
-                                    onChange={this.handleChange}
-                                    />
+                                    >{rec.usrInput}
+                                    </p>
+                                </div>
                                 </div>
                             </div>
-                            <div className="bot-image-wrapper">
-                                <img style={{
-                                    'height': '30px' ,
-                                    'width': '30px', 
-                                    'borderRadius': '20px'
-                                    }} src={image}
-                                    />
+                        ))}
+                        <div className="input-wrapper">
+                            <div className="input-text-wrapper">
+                                <a className="submit-icon" 
+                                onClick={this.handleSubmit} 
+                                >
+                                <FontAwesomeIcon
+                                icon={faShareSquare}
+                                />
+                                </a>
+                                <textarea 
+                                className="input-box" 
+                                type="text" name="usrInput" ref="usrInput"
+                                />
                             </div>
-                            <div className="chat-box-response-wrapper">
-                                <p className="bot-response">
-                                {this.state.bot.botResponse}
-                                </p>
-                            </div>
-                        </form>
-                </div>
+                        </div>
+                        <div className="bot-image-wrapper">
+                            <img style={{
+                                'height': '30px' ,
+                                'width': '30px', 
+                                'borderRadius': '20px'
+                                }} src={image}
+                                />
+                        </div>
+                        <div className="chat-box-response-wrapper">
+                        { this.state.typing == true ?
+                        (
+                        this.botTyping()
+                        ) : (
+                            this.botNotTyping()
+                        )
+                        }
+                        </div>
+                    </form>
             </div>
         )
     }
 }
+
